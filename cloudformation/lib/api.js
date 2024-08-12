98e861ff0c3ed598b49609cf74fa750135d5fd67
ingalls <nick@ingalls.ca>
2024-08-12 12:12:04 -0700

    Add SG Ingress

--- cloudformation/lib/api.js
+++ cloudformation/lib/api.js
@@ -1,5 +1,21 @@
 import cf from '@openaddresses/cloudfriend';
 
+const PORTS = [{
+    Port: 9997,
+},{
+    Port: 8554,
+},{
+    Port: 8322,
+},{
+    Port: 1935,
+},{
+    Port: 8888,
+},{
+    Port: 8889,
+},{
+    Port: 8890,
+}]
+
 export default cf.merge(
     {
         Resources: {
@@ -8,6 +24,85 @@ export default cf.merge(
                 Properties: {
                     LogGroupName: cf.stackName,
                     RetentionInDays: 7
+                }
+            },
+            ELB: {
+                Type: 'AWS::ElasticLoadBalancingV2::LoadBalancer',
+                Properties: {
+                    Name: cf.stackName,
+                    Type: 'network',
+                    SecurityGroups: [cf.ref('ELBSecurityGroup')],
+                    LoadBalancerAttributes: [{
+                        Key: 'idle_timeout.timeout_seconds',
+                        Value: 4000
+                    },{
+                        Key: 'connection_logs.s3.enabled',
+                        Value: true
+                    },{
+                        Key: 'connection_logs.s3.bucket',
+                        Value: cf.importValue(cf.join(['coe-elb-logs-', cf.ref('Environment'), '-bucket']))
+                    },{
+                        Key: 'connection_logs.s3.prefix',
+                        Value: cf.stackName
+                    },{
+                        Key: 'access_logs.s3.enabled',
+                        Value: true
+                    },{
+                        Key: 'access_logs.s3.bucket',
+                        Value: cf.importValue(cf.join(['coe-elb-logs-', cf.ref('Environment'), '-bucket']))
+                    },{
+                        Key: 'access_logs.s3.prefix',
+                        Value: cf.stackName
+                    }],
+                    Subnets:  [
+                        cf.importValue(cf.join(['coe-vpc-', cf.ref('Environment'), '-subnet-public-a'])),
+                        cf.importValue(cf.join(['coe-vpc-', cf.ref('Environment'), '-subnet-public-b']))
+                    ]
+                }
+            },
+            ELBSecurityGroup: {
+                Type : 'AWS::EC2::SecurityGroup',
+                Properties : {
+                    Tags: [{
+                        Key: 'Name',
+                        Value: cf.join('-', [cf.stackName, 'elb-sg'])
+                    }],
+                    GroupName: cf.join('-', [cf.stackName, 'elb-sg']),
+                    GroupDescription: 'Allow Access to ELB',
+                    SecurityGroupIngress: PORTS.map((port) => {
+                        return {
+                            CidrIp: '0.0.0.0/0',
+                            IpProtocol: 'tcp',
+                            FromPort: port.Port,
+                            ToPort: port.Port
+                        }
+                    }),
+                    VpcId: cf.importValue(cf.join(['coe-vpc-', cf.ref('Environment'), '-vpc']))
+                }
+            },
+            ListenerRTSP: {
+                Type: 'AWS::ElasticLoadBalancingV2::Listener',
+                Properties: {
+                    DefaultActions: [{
+                        Type: 'forward',
+                        TargetGroupArn: cf.ref('TargetGroupRTSP')
+                    }],
+                    LoadBalancerArn: cf.ref('ELB'),
+                    Port: 8554,
+                    Protocol: 'TCP'
+                },
+            },
+            TargetGroupRTSP: {
+                Type: 'AWS::ElasticLoadBalancingV2::TargetGroup',
+                DependsOn: 'ELB',
+                Properties: {
+                    Port: 8554,
+                    Protocol: 'TCP',
+                    TargetType: 'ip',
+                    VpcId: cf.importValue(cf.join(['coe-vpc-', cf.ref('Environment'), '-vpc'])),
+                    Matcher: {
+                        HttpCode: '200,202,302,304'
+                    }
                 }
             },
             TaskDefinition: {
@@ -116,6 +211,51 @@ export default cf.merge(
                             }]
                         }
                     }]
+                }
+            },
+            Service: {
+                Type: 'AWS::ECS::Service',
+                Properties: {
+                    ServiceName: cf.join('-', [cf.stackName, 'Service']),
+                    Cluster: cf.join(['coe-ecs-', cf.ref('Environment')]),
+                    TaskDefinition: cf.ref('TaskDefinition'),
+                    LaunchType: 'FARGATE',
+                    PropagateTags: 'SERVICE',
+                    DesiredCount: 1,
+                    NetworkConfiguration: {
+                        AwsvpcConfiguration: {
+                            AssignPublicIp: 'ENABLED',
+                            SecurityGroups: [cf.ref('ServiceSecurityGroup')],
+                            Subnets:  [
+                                cf.importValue(cf.join(['coe-vpc-', cf.ref('Environment'), '-subnet-public-a'])),
+                                cf.importValue(cf.join(['coe-vpc-', cf.ref('Environment'), '-subnet-public-b']))
+                            ]
+                        }
+                    },
+                    LoadBalancers: [{
+                        ContainerName: 'api',
+                        ContainerPort: 8554,
+                        TargetGroupArn: cf.ref('TargetGroup8554')
+                    }]
+                }
+            },
+            ServiceSecurityGroup: {
+                Type: 'AWS::EC2::SecurityGroup',
+                Properties: {
+                    Tags: [{
+                        Key: 'Name',
+                        Value: cf.join('-', [cf.stackName, 'ec2-sg'])
+                    }],
+                    GroupDescription: 'Allow access to TAK ports',
+                    VpcId: cf.importValue(cf.join(['coe-vpc-', cf.ref('Environment'), '-vpc'])),
+                    SecurityGroupIngress: PORTS.map((port) => {
+                        return {
+                            CidrIp: '0.0.0.0/0',
+                            IpProtocol: 'tcp',
+                            FromPort: port.Port,
+                            ToPort: port.Port
+                        }
+                    }),
                 }
             },
         },
