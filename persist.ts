9bc33c4996954d9db6adecb0b3d2d4450cb3c077
ingalls <nick@ingalls.ca>
2024-10-30 10:11:44 -0600

    Merge Paths

--- persist.ts
+++ persist.ts
@@ -3,15 +3,54 @@ import YAML from 'yaml';
 
 cron.schedule('0,10,20,30,40,50 * * * * *', async () => {
     try {
-        const res = await fetch('http://localhost:9997/v3/config/global/get', {
-            method: 'GET',
-            headers: {
-                Authorization: `Basic ${Buffer.from('management:' + process.env.MANAGEMENT_PASSWORD)}`
-            }
-        })
+        const base = await globalConfig();
+        const paths = await globalPaths();
 
-        console.error(JSON.stringify(res));
+        base.paths = paths;
+
+        console.log(YAML.stringify(base))
     } catch (err) {
         consoe.error(err);
     }
 });
+
+async function globalPaths(): any {
+    let page = -1;
+    let res: any;
+
+    const paths = [];
+
+    do {
+        ++page;
+
+        const url = new URL('http://localhost:9997/v3/config/paths/list');
+        url.searchParams.append('itemsPerPage', '1000');
+        url.searchParams.append('page', String(page));
+
+        const res = await fetch(url, {
+            method: 'GET',
+            headers: {
+                Authorization: `Basic ${Buffer.from('management:' + process.env.MANAGEMENT_PASSWORD).toString('base64')}`
+            }
+        })
+
+        const body = await res.json();
+
+        paths.push(...body.items);
+    } while (res.itemCount > page * 1000)
+
+    return paths;
+}
+
+async function globalConfig(): any {
+    const res = await fetch('http://localhost:9997/v3/config/global/get', {
+        method: 'GET',
+        headers: {
+            Authorization: `Basic ${Buffer.from('management:' + process.env.MANAGEMENT_PASSWORD).toString('base64')}`
+        }
+    })
+
+    const body = await res.json();
+
+    return body;
+}
